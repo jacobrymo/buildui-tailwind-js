db17a5991ff504faf164b56e5e25bdb1cecd0a43
Jacob Morrison <jacob.ryan.morrison@gmail.com>
2024-05-13 09:40:42 -0500

    Fix toast, add animation

--- app/ui/components/Toast.jsx
+++ app/ui/components/Toast.jsx
@@ -2,44 +2,66 @@
 import * as RadixToast from '@radix-ui/react-toast';
 import Button from './Button';
 import { XMarkIcon } from '@heroicons/react/20/solid';
-import { useState } from 'react';
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Toast = () => {
-  let [toasts, setToasts] = useState([{ id: '', message: '' }]);
+  const [open, setOpen] = React.useState(false);
+  let [toasts, setToasts] = React.useState([]);
+
   return (
     <>
       <Button
         onClick={() => {
           setToasts([
             ...toasts,
-            { id: window.crypto.randomUUID(), message: getRandomMessage() },
+            {
+              id: window.crypto.randomUUID(),
+              message: getRandomMessage(),
+            },
           ]);
         }}
       >
-        This doesn&apos;t work yet
+        Toast please!
       </Button>
-
       <RadixToast.Provider>
-        {toasts.map((toast) => {
-          {
-            console.log(toast);
-          }
-          <RadixToast.Root
-            key={toast.id}
-            className="flex w-screen items-center gap-4 rounded-xl bg-gray-400/20 py-2 pe-3 ps-4 backdrop-blur-lg md:w-fit"
-          >
-            {
-              <RadixToast.Description className="text-wrap">
-                {toast.message}
-              </RadixToast.Description>
-            }
-            <RadixToast.Close className="grid size-12 transform place-items-center rounded-md hover:scale-[1.04] hover:bg-gray-600/20 hover:text-gray-200 md:size-8">
-              <XMarkIcon className="size-6 " />
-            </RadixToast.Close>
-          </RadixToast.Root>;
-        })}
+        <AnimatePresence>
+          {toasts.map((toast) => (
+            <RadixToast.Root
+              key={toast.id}
+              className="flex items-center justify-between gap-4 rounded-xl bg-gray-400/20 py-2 pe-3 ps-4 backdrop-blur-lg"
+              onOpenChange={() => {
+                setToasts(toasts.filter((t) => t.id !== toast.id));
+              }}
+              asChild
+              forceMount
+            >
+              <motion.li
+                initial={{ y: 64, filter: 'blur(8px)' }}
+                animate={{ y: 0, filter: 'blur(0px)' }}
+                exit={{
+                  opacity: 0,
+                  zIndex: -1,
+                  filter: 'blur(24px)',
+                  transition: { duration: 0.1 },
+                }}
+                whileTap={{ scale: 0.996 }}
+                whileHover={{ scale: 1.016 }}
+                transition={{ type: 'spring', bounce: 0, duration: 0.64 }}
+                layout
+              >
+                <RadixToast.Description className="text-wrap">
+                  {toast.message}
+                </RadixToast.Description>
+                <RadixToast.Close className="grid size-12 transform place-items-center rounded-md hover:scale-[1.04] hover:bg-gray-600/20 hover:text-gray-200 md:size-8">
+                  <XMarkIcon className="size-6 " />
+                </RadixToast.Close>
+              </motion.li>
+            </RadixToast.Root>
+          ))}
+        </AnimatePresence>
 
-        <RadixToast.Viewport className="fixed right-0 top-0 m-4 flex w-40 flex-col gap-1 md:max-w-md" />
+        <RadixToast.Viewport className="fixed bottom-4 left-0 right-0 z-10 flex w-screen flex-col items-center gap-2" />
       </RadixToast.Provider>
     </>
   );
@@ -68,7 +90,6 @@ function getRandomMessage() {
     'Travel itinerary confirmed.',
     'Battery low: 10% remaining.',
   ];
-
   const randomIndex = Math.floor(Math.random() * notifications.length);
 
   return notifications[randomIndex];
