06b39c2f191320606c5a6232daae0206544d8742
Jacob Morrison <jacob.ryan.morrison@gmail.com>
2024-05-13 10:06:13 -0500

    Finish

--- app/ui/components/Button.jsx
+++ app/ui/components/Button.jsx
@@ -1,15 +1,17 @@
 'use client';
+import React from 'react';
 
-const Button = ({ onClick, children, full }) => {
+const Button = React.forwardRef(({ onClick, children, full }, ref) => {
   return (
     <button
       onClick={onClick}
-      className={`${full ? 'w-full' : 'w-fit'} h-12 rounded-xl bg-sky-500 px-4 font-bold hover:bg-sky-500  focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-white md:h-10 md:px-3`}
+      ref={ref}
+      className={`${full ? 'w-full' : 'w-fit'} h-12 rounded-xl bg-sky-500 px-4 font-bold hover:bg-sky-500/90 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-white md:h-10 md:px-3`}
     >
       <span className="px-1">{children}</span>
     </button>
   );
-};
+});
 Button.displayName = Button;
 
 export default Button;
--- app/ui/components/Toast.jsx
+++ app/ui/components/Toast.jsx
@@ -29,7 +29,7 @@ const Toast = () => {
           {toasts.map((toast) => (
             <RadixToast.Root
               key={toast.id}
-              className="flex items-center justify-between gap-4 rounded-xl bg-gray-400/20 py-2 pe-3 ps-4 backdrop-blur-lg"
+              className="flex items-center justify-between gap-4 rounded-xl bg-gray-600/60 py-2 pe-3 ps-4 backdrop-blur-lg focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-white"
               onOpenChange={() => {
                 setToasts(toasts.filter((t) => t.id !== toast.id));
               }}
@@ -53,7 +53,7 @@ const Toast = () => {
                 <RadixToast.Description className="text-wrap">
                   {toast.message}
                 </RadixToast.Description>
-                <RadixToast.Close className="grid size-12 transform place-items-center rounded-md hover:scale-[1.04] hover:bg-gray-600/20 hover:text-gray-200 md:size-8">
+                <RadixToast.Close className="grid size-12 transform place-items-center rounded-md hover:scale-[1.04] hover:bg-gray-600/20 hover:text-gray-200 focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-white md:size-8">
                   <XMarkIcon className="size-6 " />
                 </RadixToast.Close>
               </motion.li>
@@ -61,7 +61,7 @@ const Toast = () => {
           ))}
         </AnimatePresence>
 
-        <RadixToast.Viewport className="fixed bottom-4 left-0 right-0 z-10 flex w-screen flex-col items-center gap-2" />
+        <RadixToast.Viewport className="fixed bottom-0 left-0 right-0 z-10 flex w-screen flex-col items-center gap-2 rounded-xl focus-visible:outline focus-visible:outline-offset-4 focus-visible:outline-white" />
       </RadixToast.Provider>
     </>
   );
